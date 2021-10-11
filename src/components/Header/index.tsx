c2af38c3a570ba49618cafbb3df23b4ba1c3ed94
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-11 11:10:47 -0300

    feat: criação do FormContext e da estilização do Step1

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const Header = () => {
+    return (
+        <C.Container>
+            <h1>Cadastro de Desenvolvedor</h1>
+            <p>Faça seu cadastro na lista e receba um emprego instantâneamente.</p>
+        </C.Container>
+    );
+}
--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,9 +1,31 @@
 import * as C from './styles';
+import {Theme} from '../../components/Theme'; 
 
 export const FormStep1 = () => {
+
+    const handleNextStep = () => {
+        
+    }
+
     return (
-        <C.Container>
-            Passo 1
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo.</p>
+
+                <hr/>
+
+                <label>
+                    Seu nome completo
+                    <input 
+                        type="text"
+                        autoFocus
+                    />
+                </label>
+
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
     );
 }
