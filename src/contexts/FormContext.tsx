e18de0323a266c402e7897802e2e7c3a32249cdb
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-11 11:30:35 -0300

    refactor: armazenando dados da page FormStep1 e realizando validação na página

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -1,30 +1,24 @@
 // Context, Reducer, Provider, Hook
 import { createContext, ReactNode, useContext, useReducer } from 'react';
 
-
 type State = {
     currentStep: number;
-    name: string,
+    name: string;
     level: 0 | 1;
     email: string;
     github: string;
 }
-
-
-type Action = { 
+type Action = {
     type: FormActions;
     payload: any;
 };
-
-
 type ContextType = {
     state: State;
     dispatch: (action: Action) => void;
 }
-
 type FormProviderProps = {
     children: ReactNode
-}
+};
 
 const initialData: State = {
     currentStep: 0,
@@ -34,10 +28,8 @@ const initialData: State = {
     github: ''
 }
 
-
 // Context
 const FormContext = createContext<ContextType | undefined>(undefined);
-
 
 // Reducer
 export enum FormActions {
@@ -52,7 +44,7 @@ const formReducer = (state: State, action: Action) => {
         case FormActions.setCurrentStep:
             return {...state, currentStep: action.payload};
         case FormActions.setName:
-            return {...state, name: action.payload}
+            return {...state, name: action.payload};
         case FormActions.setLevel:
             return {...state, level: action.payload};
         case FormActions.setEmail:
@@ -64,13 +56,10 @@ const formReducer = (state: State, action: Action) => {
     }
 }
 
-
 // Provider
 export const FormProvider = ({children}: FormProviderProps) => {
-
     const [state, dispatch] = useReducer(formReducer, initialData);
     const value = { state, dispatch };
-
     return (
         <FormContext.Provider value={value}>
             {children}
@@ -78,11 +67,11 @@ export const FormProvider = ({children}: FormProviderProps) => {
     );
 }
 
-
 // Context Hook
 export const useForm = () => {
     const context = useContext(FormContext);
     if(context === undefined) {
         throw new Error('useForm precisa ser usado dentro do FormProvider');
     }
+    return context;
 }
--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,10 +1,37 @@
+import { useHistory } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import {Theme} from '../../components/Theme'; 
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
 
+    const history = useHistory();
+    const { state, dispatch } = useForm();
+
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, [])
+
+
     const handleNextStep = () => {
+        if(state.name !== '') {
+            history.push('/step2');
+        } else {
+            alert("Preencha os dados");
+        }
         
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
     }
 
     return (
@@ -21,6 +48,8 @@ export const FormStep1 = () => {
                     <input 
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
 
